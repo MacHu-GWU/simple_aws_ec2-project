10949a034a7b2291a509b55ab2aab223ca04af7d
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-28 10:17:14 -0400

    Update __init__.py

--- simple_aws_ec2/__init__.py
+++ simple_aws_ec2/__init__.py
@@ -7,12 +7,3 @@ __license__ = "MIT"
 __author__ = "Sanhe Hu"
 __author_email__ = "husanhe@gmail.com"
 __github_username__ = "MacHu-GWU"
-
-try:
-    from .api import (
-        EC2InstanceStatusEnum,
-        Ec2Instance,
-        Ec2InstanceIterProxy,
-    )
-except ImportError as e:  # pragma: no cover
-    pass
